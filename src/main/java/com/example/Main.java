42f8da52ae66deb9c808dfcc867ef00f8ec3ea96
Matt Robison <matt@lightboxlogic.com.au>
2020-05-15 22:41:11 +1000

    no message

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -27,11 +27,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.WebRequest;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -56,7 +51,7 @@ public class Main {
     return "index";
   }
 
-  @RequestMapping(value = "/testing", method = RequestMethod.POST)
+  @RequestMapping("/testing")
   String testing(WebRequest request, Map<String, Object> model) {
     model.put("cool", "sweet");
 
@@ -75,38 +70,4 @@ public class Main {
 
     return "testing";
   }
-
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
-
-      model.put("records", output);
-      model.put("maybe", "sweet");
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
-    }
-  }
-
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    } else {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
-    }
-  }
-
 }
