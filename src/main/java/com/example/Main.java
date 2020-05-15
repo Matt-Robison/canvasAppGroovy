40d91cb2733aba0f220ffc0f940b25719257edd3
Matt Robison <matt@lightboxlogic.com.au>
2020-05-15 15:35:18 +1000

    no message

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -53,6 +53,11 @@ public class Main {
     return "index";
   }
 
+  @RequestMapping("/testing")
+  String testing() {
+    return "testing";
+  }
+
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
     try (Connection connection = dataSource.getConnection()) {
